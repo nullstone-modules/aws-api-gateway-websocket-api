10c124fff2a50f74fbdbaf5f47fe943b8a84bb1a
Scott Sickles <scottsickles@gmail.com>
2024-04-09 18:29:19 -0400

    no lamdba permissions needed

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,3 @@
 output "public_urls" {
   value = [aws_apigatewayv2_stage.default.invoke_url]
 }
-
-output "permissions" {
-  value = [
-    {
-      sid_prefix = "AllowGatewayAccess"
-      action     = "lambda:InvokeFunction"
-      principal  = "apigateway.amazonaws.com"
-      source_arn = "${aws_apigatewayv2_api.this.execution_arn}/*"
-    }
-  ]
-}

fc9f64b7f23a6ce3a1fd7679836020d6bd86c027
Scott Sickles <scottsickles@gmail.com>
2024-04-09 22:30:50 -0400

    finished adding the route responses to enable two-way communication

--- api.tf
+++ api.tf
@@ -16,7 +16,8 @@ resource "aws_apigatewayv2_stage" "default" {
 
 resource "aws_apigatewayv2_integration" "connect-integration" {
   api_id             = aws_apigatewayv2_api.this.id
-  integration_type   = "HTTP"
+  integration_type   = "HTTP_PROXY"
+  connection_type    = "INTERNET"
   integration_method = "POST"
   integration_uri    = "https://${local.subdomain_name}/${var.path}/websocket/connect"
 }
@@ -31,11 +32,17 @@ resource "aws_apigatewayv2_route" "connect-route" {
   route_key = "$connect"
   target    = "integrations/${aws_apigatewayv2_integration.connect-integration.id}"
 }
+resource "aws_apigatewayv2_route_response" "connect-response" {
+  api_id             = aws_apigatewayv2_api.this.id
+  route_id           = aws_apigatewayv2_route.connect-route.id
+  route_response_key = "$default"
+}
 
 
 resource "aws_apigatewayv2_integration" "default-integration" {
   api_id             = aws_apigatewayv2_api.this.id
-  integration_type   = "HTTP"
+  integration_type   = "HTTP_PROXY"
+  connection_type    = "INTERNET"
   integration_method = "POST"
   integration_uri    = "https://${local.subdomain_name}/${var.path}/websocket/default"
 }
@@ -50,11 +57,17 @@ resource "aws_apigatewayv2_route" "default-route" {
   route_key = "$default"
   target    = "integrations/${aws_apigatewayv2_integration.default-integration.id}"
 }
+resource "aws_apigatewayv2_route_response" "default-response" {
+  api_id             = aws_apigatewayv2_api.this.id
+  route_id           = aws_apigatewayv2_route.default-route.id
+  route_response_key = "$default"
+}
 
 
 resource "aws_apigatewayv2_integration" "disconnect-integration" {
   api_id             = aws_apigatewayv2_api.this.id
-  integration_type   = "HTTP"
+  integration_type   = "HTTP_PROXY"
+  connection_type    = "INTERNET"
   integration_method = "POST"
   integration_uri    = "https://${local.subdomain_name}/${var.path}/websocket/disconnect"
 }
@@ -69,11 +82,17 @@ resource "aws_apigatewayv2_route" "disconnect-route" {
   route_key = "$disconnect"
   target    = "integrations/${aws_apigatewayv2_integration.disconnect-integration.id}"
 }
+resource "aws_apigatewayv2_route_response" "disconnect-response" {
+  api_id             = aws_apigatewayv2_api.this.id
+  route_id           = aws_apigatewayv2_route.disconnect-route.id
+  route_response_key = "$default"
+}
 
 
 resource "aws_apigatewayv2_integration" "chat-integration" {
   api_id             = aws_apigatewayv2_api.this.id
-  integration_type   = "HTTP"
+  integration_type   = "HTTP_PROXY"
+  connection_type    = "INTERNET"
   integration_method = "POST"
   integration_uri    = "https://${local.subdomain_name}/${var.path}/websocket/chat"
 }
@@ -88,11 +107,17 @@ resource "aws_apigatewayv2_route" "chat-route" {
   route_key = "chat"
   target    = "integrations/${aws_apigatewayv2_integration.chat-integration.id}"
 }
+resource "aws_apigatewayv2_route_response" "chat-response" {
+  api_id             = aws_apigatewayv2_api.this.id
+  route_id           = aws_apigatewayv2_route.chat-route.id
+  route_response_key = "$default"
+}
 
 
 resource "aws_apigatewayv2_integration" "message-integration" {
   api_id             = aws_apigatewayv2_api.this.id
-  integration_type   = "HTTP"
+  integration_type   = "HTTP_PROXY"
+  connection_type    = "INTERNET"
   integration_method = "POST"
   integration_uri    = "https://${local.subdomain_name}/${var.path}/websocket/message"
 }
@@ -106,4 +131,9 @@ resource "aws_apigatewayv2_route" "message-route" {
   api_id    = aws_apigatewayv2_api.this.id
   route_key = "message"
   target    = "integrations/${aws_apigatewayv2_integration.message-integration.id}"
+}
+resource "aws_apigatewayv2_route_response" "message-response" {
+  api_id             = aws_apigatewayv2_api.this.id
+  route_id           = aws_apigatewayv2_route.message-route.id
+  route_response_key = "$default"
 }

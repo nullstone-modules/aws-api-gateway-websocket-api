7210ca4f1f632a562c87e6ca28b48b47baa93f4d
Scott Sickles <scottsickles@gmail.com>
2024-04-09 20:06:13 -0400

    fix example

--- variables.tf
+++ variables.tf
@@ -2,6 +2,6 @@ variable "path" {
   type        = string
   description = <<EOF
 The path to append to the subdomain in order to form the full URL that requests will be routed to.
-e.g. If the subdomain connected is for https://api.acme.com, and the path is /v1, then the full URL will be https://api.acme.com/v1.
+e.g. If the subdomain connected is for https://api.acme.com, and the path is v1, then the full URL will be https://api.acme.com/v1.
 EOF
 }

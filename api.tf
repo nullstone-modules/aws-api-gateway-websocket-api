0d18820a55b98b1e0ab03aecb7350c6bf044c623
Scott Sickles <scottsickles@gmail.com>
2024-04-15 16:07:41 -0400

    match throttling settings in case they matter

--- api.tf
+++ api.tf
@@ -13,6 +13,11 @@ resource "aws_apigatewayv2_stage" "default" {
   deployment_id = aws_apigatewayv2_deployment.this.id
 
   default_route_settings {
+    throttling_burst_limit = 5000
+    throttling_rate_limit  = 10000
+  }
+
+  default_route_settings {
     logging_level = "INFO"
   }
 

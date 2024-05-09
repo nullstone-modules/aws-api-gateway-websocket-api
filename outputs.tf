9386eb396711514788c830ca5f7d6e9fe802069b
Scott Sickles <scottsickles@gmail.com>
2024-05-09 09:42:18 -0400

    update the outputs to include the websocket api name

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "websocket_url" {
   value = aws_apigatewayv2_stage.default.invoke_url
 }
 
-output "websocket_arn" {
+output "websocket_api_arn" {
   value = aws_apigatewayv2_api.this.arn
+}
+
+output "websocket_api_name" {
+  value = aws_apigatewayv2_api.this.name
 }
